effc5506f10648cada0b3734a21f01b24ea8d41f
Hoang-Nhan Nguyen <nhan.nh1526@gmail.com>
2022-05-08 00:02:15 +0700

    Update serializers.py

--- Django/core/tms_api/serializers.py
+++ Django/core/tms_api/serializers.py
@@ -1,8 +1,10 @@
+from dataclasses import field
 from rest_framework import serializers
 from tms.models import Request
 
 
 class RequestSerializer(serializers.ModelSerializer):
     class Meta:
-        exclude = ['created_on', 'slug']
         model = Request
+        fields = ('owner', 'request_type', 'start_date', 'end_date', 'partial_day', 'reason',
+                  'approver', 'detail_reason', 'supervisor', 'inform_to', 'expected_approve', 'time')
