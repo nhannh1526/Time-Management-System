c632bafb14e14e5ed5b72d448c11bf823455fc50
Hoang-Nhan Nguyen <nhan.nh1526@gmail.com>
2022-05-08 17:08:46 +0700

    Update serializers.py

--- Django/core/tms_api/serializers.py
+++ Django/core/tms_api/serializers.py
@@ -1,10 +1,18 @@
-from dataclasses import field
 from rest_framework import serializers
 from tms.models import Request
 
 
 class RequestSerializer(serializers.ModelSerializer):
+    partial_day_display = serializers.CharField(
+        source='get_partial_day_display')
+
     class Meta:
         model = Request
-        fields = ('owner', 'request_type', 'start_date', 'end_date', 'partial_day', 'reason',
-                  'approver', 'detail_reason', 'supervisor', 'inform_to', 'expected_approve', 'time')
+        fields = ('id', 'owner', 'owner_name', 'request_type', 'request_type_name',
+                  'start_date', 'end_date', 'partial_day', 'partial_day_display', 'duration',
+                  'reason', 'reason_name', 'approver', 'approver_name',
+                  'detail_reason', 'supervisor', 'supervisor_name',
+                  'inform_to', 'inform_to_name', 'expected_approve', 'time')
+        read_only_fields = ('id', 'owner', 'owner_name', 'partial_day_display', 'duration',
+                            'request_type_name', 'reason_name', 'approver_name',
+                            'supervisor_name', 'inform_to_name')
